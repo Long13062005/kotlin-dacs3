5cca333eb51ba2aeaf64f46ebd86a3578e0e17cf
Long13062005 <vulong13062005@gmail.com>
2025-02-13 14:18:28 +0700

    update

--- client/app/src/androidTest/java/com/example/client/ExampleInstrumentedTest.kt
+++ client/app/src/androidTest/java/com/example/client/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.ui_dacs3
+package com.example.client
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.ui_dacs3", appContext.packageName)
+        assertEquals("com.example.client", appContext.packageName)
     }
 }
--- client/app/src/test/java/com/example/client/ExampleUnitTest.kt
+++ client/app/src/test/java/com/example/client/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.ui_dacs3
+package com.example.client
 
 import org.junit.Test
 
